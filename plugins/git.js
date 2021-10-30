f13984e9dc36c3d29de84373ff5d450d767e25c2
Mechzopikku <92675610+Mechzopikku@users.noreply.github.com>
2021-10-30 08:37:43 +0530

    Update git.js

--- plugins/git.js
+++ plugins/git.js
@@ -18,22 +18,22 @@ Asena.addCommand({pattern: 'git', fromMe: false, desc: Lang.GL}, (async (message
 
     var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
 
-    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {quoted: message.data , mimetype: Mimetype.png, caption: `*Ajuser_v2*
+    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {quoted: message.data , mimetype: Mimetype.png, caption: `*Sanu ser*
  
 
-        *Github link        _https://github.com/aju001/Ajuser_v2_*
+        *Github link        _https://github.com/Mechzopikku/Pikku_Mwon*
         
         
        *Bot making video   _https://youtu.be/dm_kVZ0m2eY_*
  
 
-       *Audio commads-1     _https://github.com/aju001/Ajuser_v2/tree/master/upload_*
+       *Audio commads-1     _https://github.com/Mechzopikku/Pikku_Mwontree/master/upload_*
  
  
-       *Audio commads-2     _https://github.com/aju001/Ajuser_v2/tree/master/uploads_*
+       *Audio commads-2     _https://github.com/Mechzopikku/Pikku_Mwontree/master/uploads_*
   
   
-      *Sticker commads      _https://github.com/aju001/Ajuser_v2/tree/master/stickers_*
+      *Sticker commads      _https://github.com/Mechzopikku/Pikku_Mwontree/master/stickers_*
 `}) 
 
 })); 
